7e848b9920763a91e2e3c0643341d37e3f3d1306
Gabriel <gmelo8720@gmail.com>
2026-03-23 14:34:49 -0300

    adiciona scripts  e

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a288b685a1fdf7df"; }
+/******/ 		__webpack_require__.h = function() { return "c4bb6fa3aaea41eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
