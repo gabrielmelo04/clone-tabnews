b50d40f7c4141965c992e49675b27f9b31b36f22
Gabriel <gmelo8720@gmail.com>
2026-05-12 12:10:53 -0300

    remove env file

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true, // Pelo docker false -> pelo neon true
+    // channelBinding: process.env.PGCHANNELBINDING,
+    // pgSslMode: process.env.PGSSLMODE,
   });
 
   try {
