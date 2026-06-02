3d0e20d45f1fdb610ec4ccc9ea909cece94b3c44
Gabriel <gmelo8720@gmail.com>
2026-06-02 09:56:23 -0300

    fix /migrations endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,37 +3,44 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const defaultMigrationsOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"), // Caminho para a pasta de migrações para windwos, linux e mac - para não dar erro coloco o join do node path
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations", // Nome da tabela onde as migrações serão registradas
-  };
-
-  if (request.method === "GET") {
-    const paddingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return response.status(200).json(paddingMigrations);
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({ error: "Method Not Allowed" });
   }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
+  let dbClient = await database.getNewClient();
 
-    await dbClient.end();
+  try {
+    const defaultMigrationsOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"), // Caminho para a pasta de migrações para windwos, linux e mac - para não dar erro coloco o join do node path
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations", // Nome da tabela onde as migrações serão registradas
+    };
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    if (request.method === "GET") {
+      const paddingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return response.status(200).json(paddingMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
 
-  return response.status(405).end(); // Retorna 405 para métodos não permitidos
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw Error("Error running migrations");
+  } finally {
+    await dbClient.end();
+  }
 }
