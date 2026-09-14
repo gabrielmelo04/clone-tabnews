c0737d3e1accfb0a72d45de6f3d02834ff4fb1a3
Gabriel <gmelo8720@gmail.com>
2026-09-14 07:55:34 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,20 +3,25 @@ import orchestrator from "tests/orchestrator.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Verifica se é uma data válida
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // Verifica se a data retornada é a mesma que foi gerada
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Verifica se é uma data válida
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // Verifica se a data retornada é a mesma que foi gerada
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0"); // Verifica se a versão do banco de dados é a esperada
-  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0); // Verifica se o número de conexões máximas é um valor positivo
-  expect(responseBody.dependencies.database.max_connections).toEqual(100); // Verifica se o número de conexões máximas é 100
-  expect(responseBody.dependencies.database.open_connections).toEqual(1); // Verifica se o número de conexões abertas é 1
+      expect(responseBody.dependencies.database.version).toEqual("16.0"); // Verifica se a versão do banco de dados é a esperada
+      expect(
+        responseBody.dependencies.database.max_connections,
+      ).toBeGreaterThan(0); // Verifica se o número de conexões máximas é um valor positivo
+      expect(responseBody.dependencies.database.max_connections).toEqual(100); // Verifica se o número de conexões máximas é 100
+      expect(responseBody.dependencies.database.open_connections).toEqual(1); // Verifica se o número de conexões abertas é 1
+    });
+  });
 });
